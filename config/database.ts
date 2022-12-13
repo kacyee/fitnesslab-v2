a8b06e59969ee347fd234643a749af94c8853da2
Patryk Puslecki <patrykpuslecki@gmail.com>
2022-12-13 12:32:01 +0100

    modified

--- config/database.ts
+++ config/database.ts
@@ -1,16 +1,17 @@
-import { Sequelize } from 'sequelize-typescript';
-import 'dotenv/config';
-import { User, Training, UserHasTrainings } from '../models';
+import { Sequelize } from "sequelize-typescript";
+import "dotenv/config";
+import { User, Training, UserHasTrainings } from "../models";
 
 const basepath = process.cwd();
 const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  dialect: 'postgres',
+  dialect: "postgres",
   models: [User, Training, UserHasTrainings],
   repositoryMode: false,
 });
-console.log(basepath + '/models');
+console.log(basepath + "/models");
 
 export default sequelize;
--- dist/config/database.js
+++ dist/config/database.js
@@ -5,12 +5,13 @@ require("dotenv/config");
 const models_1 = require("../models");
 const basepath = process.cwd();
 const sequelize = new sequelize_typescript_1.Sequelize({
+    host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    dialect: 'postgres',
+    dialect: "postgres",
     models: [models_1.User, models_1.Training, models_1.UserHasTrainings],
     repositoryMode: false,
 });
-console.log(basepath + '/models');
+console.log(basepath + "/models");
 exports.default = sequelize;
